ff01e30e850c07dced33ffc675d58c8b1d84837a
Kim Jiyeon <iop0357@naver.com>
2025-03-05 15:28:14 +0900

    객체, 함수

--- 30~35th class/app.js
+++ 30~35th class/app.js
@@ -1,4 +1,30 @@
-let greetingText = 'Hi I\'m Max!';
-alert(greetingText);
-greetingText = 'Hi I\'m Jiyeon!';
-alert(greetingText);
+let age = 32;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = { 
+    title: "Developer", 
+    place: "New York", 
+    salary: 50000 
+};
+
+alert(hobbies[0]);
+alert(job.title);
+
+function countAge(){
+    return age + 18;
+}
+
+alert(countAge());
+
+let onlineCourse = {
+    name:'Node.js project',
+    price:50000,
+    goals: ['Learn HTML&CSS','Learn Java Script','Lean Node.js']
+};
+
+function getListItem(array, arrayIndex){
+    let arrayElement = array[arrayIndex];
+    return arrayElement;
+}
+
+alert(getListItem(onlineCourse.goals,1));
