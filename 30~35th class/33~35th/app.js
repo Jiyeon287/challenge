d4b7d7df179d6cc6ebdfd8316f42fe7308e0525d
Kim Jiyeon <iop0357@naver.com>
2025-03-05 18:00:16 +0900

    document 내 특정값 가져오기

--- 30~35th class/33~35th/app.js
+++ 30~35th class/33~35th/app.js
@@ -4,4 +4,13 @@ console.log(window);
 
 // console.log(window.document);
 console.log(document);
+console.dir(document);
+
+// document.body.children[1].children[0].href = 'https://google.com';
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('p a');
+anchorElement.href = 'https://www.naver.com';
 
