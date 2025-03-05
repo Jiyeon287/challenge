6a9dc78c995d09ca7f40f2fc97dc3f8b7a1df7b7
Kim Jiyeon <iop0357@naver.com>
2025-03-05 19:11:09 +0900

    create, delete, move element

--- 30~35th class/33~35th/app.js
+++ 30~35th class/33~35th/app.js
@@ -14,3 +14,26 @@ anchorElement.href = 'https://google.com';
 anchorElement = document.querySelector('p a');
 anchorElement.href = 'https://www.naver.com';
 
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://youtube.com';
+newAnchorElement.textContent = 'This leads to Google!';
+let firstParagraph = document.querySelector('p');
+firstParagraph.append(newAnchorElement);
+
+
+//delete
+let firstH1Element = document.querySelector('h1');
+firstH1Element.remove();
+firstH1Element.parentElement.removeChild(firstH1Element);
+
+//move
+firstParagraph.parentElement.append(firstParagraph);
+
+//innerHTML
+// firstParagraph.textContent = 'Hi! This is <strong>important!</strong>';
+firstParagraph.innerHTML = 'Hi! This is <strong>important!</strong>';
+
+
+
+
+
