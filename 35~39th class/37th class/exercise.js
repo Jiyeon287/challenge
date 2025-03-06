91f61a3b3b08adbc4bdf2e8dfc05a12c04e86e92
Kim Jiyeon <iop0357@naver.com>
2025-03-06 15:14:25 +0900

    내가 푼 답안지(넘 어렵당 ㅠ)

--- 35~39th class/37th class/exercise.js
+++ 35~39th class/37th class/exercise.js
@@ -3,15 +3,33 @@
 // 1) Select the two <button> elements and store them in two different variables.
 //    - Select the first button without adding or using any "id"
 //    - Select the second button by using an "id"
+const firstButton = document.querySelector('button');
+const secondButton = document.getElementById('second-button');
+
 // 2) Add "click" event listener to both buttons (with two different functions).
 //    The functions should "console.dir()" the clicked buttons.
 //    - Output the first button by using the variable in which it's stored
 //    - Output the second button WITHOUT using the variable in which it's stored
+function clickButton1() {
+    console.dir(firstButton);
+}
+
+function clickButton2(event) {
+    console.dir(event.target);
+}
+
+firstButton.addEventListener('click', clickButton1);
+secondButton.addEventListener('click', clickButton2);
+
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
 //    (first and third paragraph)
 //    - Select BOTH paragraphs by drilling into the document and "navigating" to the
 //      mentioned elements
 //    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
+let firstParagraphs = document.body.children[1]
+console.log(firstParagraphs);
+// let thirdParagraphs = document.body.children[]
+
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
 //    - The second button changes the background color of the first paragraph to blue
