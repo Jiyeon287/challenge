ad542e24e7c0e2846a3a279800bc0033b7b11d3d
Kim Jiyeon <iop0357@naver.com>
2025-03-09 14:33:08 +0900

    37강까지 완강

--- 35~37th class/37th class/exercise.js
+++ 35~37th class/37th class/exercise.js
@@ -10,18 +10,18 @@ const secondButton = document.getElementById('second-button');
 //    The functions should "console.dir()" the clicked buttons.
 //    - Output the first button by using the variable in which it's stored
 //    - Output the second button WITHOUT using the variable in which it's stored
-//버튼에서 꺼내기
-function clickButton1() {
-    console.dir(firstButton);
-}
+// //버튼에서 꺼내기
+// function clickButton1() {
+//     console.dir(firstButton);
+// }
 
-//event에서 꺼내기
-function clickButton2(event) {
-    console.dir(event.target);
-}
+// //event에서 꺼내기
+// function clickButton2(event) {
+//     console.dir(event.target);
+// }
 
-firstButton.addEventListener('click', clickButton1);
-secondButton.addEventListener('click', clickButton2);
+// firstButton.addEventListener('click', clickButton1);
+// secondButton.addEventListener('click', clickButton2);
 
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
 //    (first and third paragraph)
@@ -33,10 +33,23 @@ console.log(firstParagraphs);
 let thirdParagraphs = firstParagraphs.nextElementSibling.nextElementSibling;
 console.log(thirdParagraphs);
 
-// let thirdParagraphs = document.body.children[]
-
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
 //    - The second button changes the background color of the first paragraph to blue
+function removeThirdParagraph() {
+    thirdParagraphs.remove();
+}
+
+function changeFirstParagraph(event) {
+    //직접 css 변경
+    // firstParagraphs.style.backgroundColor = 'blue';
+
+    //css에 클래스를 추가하고 그 클래스를 해당요소에 추가
+    firstParagraphs.className = 'blue-bg';
+}
+
+firstButton.addEventListener('click', removeThirdParagraph);
+secondButton.addEventListener('click', changeFirstParagraph);
+
 // 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
 //    Note: You'll have to add those classes to the styles.css file first!
