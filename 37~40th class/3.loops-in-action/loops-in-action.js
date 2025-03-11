8ad9b5063fb66ff91ee4b7c53b7f97303e40c79a
Kim Jiyeon <iop0357@naver.com>
2025-03-11 14:19:52 +0900

    첫번째: 버튼 클릭시 합계가 노출되어야함

---
+++ 37~40th class/3.loops-in-action/loops-in-action.js
@@ -0,0 +1,22 @@
+// const button = document.getElementById('calculated-sum-button');
+//calculator id를 가진 section의 button
+const buttonElement = document.querySelector('#calculator button')
+
+function calculataSum(){
+    const userNumber = document.getElementById('user-number');
+
+    //다른 객체는 상수로 받아도 되는데 for에 사용되는건 let 사용해야함
+    let sumNumber = 0;
+    for(let i=0; i<=userNumber.value; i++){
+        sumNumber = sumNumber + i;
+    }
+    const sumParagraph = document.getElementById('calculated-sum');
+    sumParagraph.textContent = sumNumber;
+    //중요~!
+    sumParagraph.style.display = 'block';
+}
+
+
+buttonElement.addEventListener('click', calculataSum);
+
+
