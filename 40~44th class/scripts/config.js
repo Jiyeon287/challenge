af9a94e8af92ff89dcc9e3d12d32e646547bab5b
Kim Jiyeon <iop0357@naver.com>
2025-03-13 21:49:28 +0900

    오버레이창에서 유효성검사 추가중

--- 40~44th class/scripts/config.js
+++ 40~44th class/scripts/config.js
@@ -7,3 +7,20 @@ function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
 }
+
+function savePlayerConfig(event) {
+  //유효성 검사, 데이터를 변경(서버로 전송하는 것은 아니고 사이트의 메타데이터를 변경)
+  event.preventDefault();
+  //객체 인스턴스화
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get('playername').trim();
+
+  //html에서 required를 js로 변환가능
+  if(!enteredPlayername) { // enteredPlayername === ''
+    errorsOutputElement.textContent = 'Please enter a valid name!'
+
+  }
+
+
+
+}
