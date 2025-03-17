3bfb5f80fc4b14be25f05a757c3e2595f0172c61
Kim Jiyeon <iop0357@naver.com>
2025-03-17 13:44:05 +0900

    o x 표시

--- 40~44th class/scripts/game.js
+++ 40~44th class/scripts/game.js
@@ -6,4 +6,30 @@ function startNewGame() {
     }
 
     gameAreaElement.style.display = 'block';
+    activePlayerNameElement.textContent = players[activePlayer].name;
+
+
+}
+
+function switchPlayer(){
+    if (activePlayer === 0){
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event){
+    if(event.target.tagName !== 'LI') {
+        return;
+    }
+
+    const selectedField = event.target;
+
+    // selectedField.textContent = players[]
+    event.target.textContext = players[activePlayer].symbol; // 초기값 players[0]
+    console.log(event.target.name + players[activePlayer].symbol);
+    event.target.classList.add('disabled');
+    switchPlayer();
 }
