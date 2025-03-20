1a30bb18bdb9512571ae8d832ddd7234c28eebc8
Kim Jiyeon <iop0357@naver.com>
2025-03-20 15:43:34 +0900

    input form -> json 파일로 저장

--- 46-49th class/app.js
+++ 46-49th class/app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -17,6 +19,23 @@ app.get('/', function (request, response){
 app.post('/store-user', function(request, response){
   const userName = request.body.username;
   console.log(userName);
+
+  //users.json 파일 경로 가져오기
+  const filePath = path.join(__dirname, 'data', 'users.json');
+  
+  //users.json 파일 읽어들이기
+  const fileData = fs.readFileSync(filePath);
+  
+  //users.json 내에 있는 유저들 이름 가져오기(TEXT -> JSON)
+  const existingUsers = JSON.parse(fileData);
+  
+  //그 유저들에 New UserName 추가
+  existingUsers.push(userName);
+
+  //users.json 파일에 TEXT 형식으로 추가(JSON -> TEXT)
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
+
   response.send('<h1>Username stored!</h1>')
 });
 app.listen(3000);
