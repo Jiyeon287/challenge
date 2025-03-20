8ff172b04add83a6dbb480f38952de062c016499
Kim Jiyeon <iop0357@naver.com>
2025-03-20 15:29:02 +0900

    express로 변경한 후 post방식까지 추가(input)

--- 46-49th class/app.js
+++ 46-49th class/app.js
@@ -2,16 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/currenttime', function (request, response){
   response.send("<h1>"+ new Date().toISOString() +"</h1>");
 
-})
+});
 
 app.get('/', function (request, response){
-  response.send("<h1>Hello World!</h1>");
+  response.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
 
-})
+});
 
+app.post('/store-user', function(request, response){
+  const userName = request.body.username;
+  console.log(userName);
+  response.send('<h1>Username stored!</h1>')
+});
 app.listen(3000);
 
 
