c17dafbd427c4e0d08a08a89687abe6d951b173a
Kim Jiyeon <iop0357@naver.com>
2025-03-20 16:09:56 +0900

    저장한 데이터 동적으로 읽어오기(get /users)

--- 46-49th class/app.js
+++ 46-49th class/app.js
@@ -1,43 +1,55 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/currenttime', function (request, response){
-  response.send("<h1>"+ new Date().toISOString() +"</h1>");
-
+app.get("/currenttime", function (request, response) {
+  response.send("<h1>" + new Date().toISOString() + "</h1>");
 });
 
-app.get('/', function (request, response){
-  response.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
-
+app.get("/", function (request, response) {
+  response.send(
+    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>'
+  );
 });
 
-app.post('/store-user', function(request, response){
+app.post("/store-user", function (request, response) {
   const userName = request.body.username;
   console.log(userName);
 
   //users.json 파일 경로 가져오기
-  const filePath = path.join(__dirname, 'data', 'users.json');
-  
+  const filePath = path.join(__dirname, "data", "users.json");
+
   //users.json 파일 읽어들이기
   const fileData = fs.readFileSync(filePath);
-  
+
   //users.json 내에 있는 유저들 이름 가져오기(TEXT -> JSON)
   const existingUsers = JSON.parse(fileData);
-  
+
   //그 유저들에 New UserName 추가
   existingUsers.push(userName);
 
   //users.json 파일에 TEXT 형식으로 추가(JSON -> TEXT)
   fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
+  response.send("<h1>Username stored!</h1>");
+});
 
-  response.send('<h1>Username stored!</h1>')
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json");
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = '<ul>';
+
+  for(const user of existingUsers){
+    responseData += '<li>' + user + '</li>';
+  }
+
+  responseData += '</ul>';
+  res.send(existingUsers);
 });
 app.listen(3000);
-
-
