3edb2063e019516ffc383c93912e013a29910133
Kim Jiyeon <iop0357@naver.com>
2025-03-20 14:05:20 +0900

    3000 port로 띄우기 성공

--- 46~49th class/app.js
+++ 46~49th class/app.js
@@ -1,3 +1,15 @@
 const userName = 'jykim';
 console.log(userName);
 
+const http = require('http');
+
+function handleRequest(request, response) {
+    response.statusCode = 200;
+    response.end('<h1>Hello World!</h1>');
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
+
+//3000포트의 서버를 리슨
