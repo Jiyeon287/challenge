0626197338bc0a7b5d9e1829d2a1b0d20fd993f5
Kim Jiyeon <iop0357@naver.com>
2025-03-20 14:28:56 +0900

    node js로 간단한 예시 만들기

--- 46~49th class/app.js
+++ 46~49th class/app.js
@@ -1,11 +1,16 @@
-const userName = 'jykim';
+const userName = "jykim";
 console.log(userName);
 
-const http = require('http');
+const http = require("http");
 
 function handleRequest(request, response) {
+  if (request.url === "/cureenttime") {
     response.statusCode = 200;
-    response.end('<h1>Hello World!</h1>');
+    response.end("<h1>"+ new Date().toISOString() +"</h1>");
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<h1>Hello World!</h1>");
+  }
 }
 
 const server = http.createServer(handleRequest);
