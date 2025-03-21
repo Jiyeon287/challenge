c3c61e7be6f8ecd50c30245a7797f98118becbd7
Kim Jiyeon <iop0357@naver.com>
2025-03-21 14:28:47 +0900

    html 내 경로 -> url로 변경

--- 49-51th class/app.js
+++ 49-51th class/app.js
@@ -5,12 +5,22 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(){
-
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
 });
 
 app.get('/restaurants', function(req, res){
     const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
     res.sendFile(htmlFilePath);
 });
+
+app.get('/confirm', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+});
+
+app.get('/about', function(req, res){
+    const htmlFilePath = path.join(__dirname,'views', 'about.html')
+});
+
 app.listen(3000);
 
